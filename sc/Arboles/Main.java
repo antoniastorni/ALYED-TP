b49ff769b855fe8522f9e7c80a1fa7d8b7d87308
Solange Blacutt <solyblacutt@gmail.com>
2021-04-21 18:26:57 -0300

    s

--- sc/Arboles/Main.java
+++ sc/Arboles/Main.java
@@ -3,10 +3,17 @@ package Arboles;
 public class Main {
 
     public static void main(String[] args) {
-        DoubleNode<String> newNode = new DoubleNode<>();
-        newNode.
-        BinaryTree<String> myTree = new BinaryTree<>("h");
-        myTree.
+        DoubleNode<String> tleft = new DoubleNode<>("j");
+        DoubleNode<String> tright = new DoubleNode<>("k");
+        tleft.left = new DoubleNode<>("w");
+        tright.left = new DoubleNode<>("s");
+        tright.right = new DoubleNode<>("t");
+        tleft.left.left = new DoubleNode<>("f");
+        tleft.left.right = new DoubleNode<>("m");
+        tright.left.left = new DoubleNode<>("a");
+        tright.left.right = new DoubleNode<>("b");
+        BinaryTree<String> myTree = new BinaryTree<>("h", tleft, tright);
+
 
     }
 }
