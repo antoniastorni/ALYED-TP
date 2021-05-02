40221b0432214b6682a3b9ce3897fd5cc4095e4a
Solange Blacutt <solyblacutt@gmail.com>
2021-05-02 14:18:52 -0300

    s

---
+++ sc/Queue/StackInterface.java
@@ -0,0 +1,12 @@
+package Queue;
+
+public interface StackInterface<T> {
+    
+    boolean isEmpty();
+    T peek();
+    void pop() throws IsEmptyException;
+    void stack(T element);
+    int size();
+    void empty();
+
+}
