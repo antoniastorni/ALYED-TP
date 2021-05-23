de06230f09d1f6a7c284194d6e5593cbca8cd1fe
Solange Blacutt <solyblacutt@gmail.com>
2021-05-23 19:59:00 -0300

    :B

--- sc/TpComparacion/Main.java
+++ sc/TpComparacion/Main.java
@@ -16,6 +16,29 @@ public class Main {
 
         long finalmilis = System.currentTimeMillis();
         System.out.println("Tardo en crearse:" + (finalmilis-iniciomilis) + "milisegundos");
+
+
+
+
+        //d
+        int[] toSearch = new int[10];
+        int j = 0;
+        for (int i = 0; i < toSearch.length; i++) {
+                toSearch[i] = j;
+                j =+ 100;
+            }
+
+        for (int i = 0; i < toSearch.length; i++) {
+            bst.search(toSearch[i]);
+            avl.search(toSearch[i]);
+            rb.search(toSearch[i]);
+            System.out.println("To search: " + toSearch[i]);
+            System.out.println("Binary Search Tree: " + bst.getContadorDeBusqueda());
+            System.out.println("AVL Tree: " + avl.getContadorDeBusqueda());
+            System.out.println("Red Black Tree: " + rb.getContadorDeBusqueda());
+
+        }
+        }
     }
 
 }
